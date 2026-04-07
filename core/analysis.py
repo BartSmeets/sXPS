881e69ab57a77b4fae5c57bdf95bcc05c624bbdf
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2026-04-07 15:46:42 +0200

    add fit to data class

--- core/analysis.py
+++ core/analysis.py
@@ -1,4 +1,6 @@
 import numpy as np
+from lmfitxps import models
+import lmfit
 
 class data:
     def __init__(self, file, worksheet):
@@ -21,94 +23,58 @@ class data:
         df.columns = ['Binding Energy (eV)', 'Counts / s']
         
         # Convert to numpy
-        self.energy = self.df['Binding Energy (eV)'].to_numpy(dtype=np.float64)
-        self.counts = self.df['Counts / s'].to_numpy(dtype=np.float64)
+        self.energy = df['Binding Energy (eV)'].to_numpy(dtype=np.float64)
+        self.counts = df['Counts / s'].to_numpy(dtype=np.float64)
+
+        # Prepare fit
+        self.models = {'bg': models.ShirleyBG(prefix='shirley_')}   # Add background
+        self.pars = lmfit.Parameters()
+        ## Background parameters
+        self.pars.add('shirley_k', value=0.002, min=0, max=5)
+        self.pars.add('shirley_const', value=self.counts[-1], min=0, max=2*np.max(self.counts))
 
         return
-    
-
-    def baseline(self, lamb=1e6, ratio=1e-6, niter=10, mask=None):
-                '''
-                arPLS Baseline Correction 
-                From Baek et al. Analyst (2014); DOI:10.1039/C4AN01061B
-                '''
-                # Initialisation
-                from scipy import sparse
-                from scipy.sparse.linalg import spsolve
-                from numpy.linalg import norm
-
-                ## Matrices
-                y = self.counts
-                N = len(y)
-                diag = np.ones(N - 2)
-                D = sparse.spdiags([diag, -2*diag, diag], [0, -1, -2], N, N-2)
-                H = lamb * D.dot(D.T)
-                w = np.ones(N)
-                W = sparse.spdiags(w, 0, N, N)
-                ## Loop
-                count = 0 
-
-                while True:
-                    # Update weights
-                    W.setdiag(w)
-                    z = spsolve(W + H, W * y)
-
-                    # Make d- for logistic function
-                    d = y - z
-                    dn = d[d < 0]
-                    ## mean and std of d-
-                    m = np.mean(dn)
-                    s = np.std(dn)
-                    ## Logistic function
-                    k = 2   # logistic steepness
-                    w_new = 1 / (1 + np.exp(k * (d - (2*s - m))/s))
-                    
-                    if mask is not None:
-                        w_new[mask] = 1e-4
-
-                    # Check conditions
-                    count += 1 
-                    if count > niter:
-                        break
-                    if norm(w - w_new)/norm(w) < ratio:
-                        break
-                    else:
-                        w = w_new
-                return z
-    
-
-class peak:
-    def __init__(self):
-        pass
-     
-    def __ulrik(x, E, F, a=0, b=0, m=0):
-        '''
-        a and b: asymmetry parameters
-        F: FWHM
-        E: peak position
-        m: Lorentzian fraction
-        '''
 
 
-        def GL(x, F, E, m):
-            return np.exp(-4*np.log(2)*(1-m)*(x-E)**2/(F**2))/(1 + 4*m*(x-E)**2/(F**2))
-        def G(x, F, E):
-            return np.exp(-4*np.log(2)*(x-E)**2/F**2)
-        def w(a, b):
-            return b*(0.7+0.3/(a+0.01))
-        def AW(x, a, F, E):
-            teller = 2*np.sqrt(np.log(2))*(x-E)
-            noemer = F - a*2*np.sqrt(np.log(2))*(x-E)
-            return np.exp(-(teller/noemer)**2)
+    def add_singlet(self, key, pos):
+        self.models[key] = models.ConvGaussianDoniachSinglett(prefix=f'{key}_')
 
-        return np.where(x>E, GL(x, F, E, m), GL(x, F, E, m) + w(a,b)*(AW(x, a, F, E)-G(x, F, E)))
-    
+        self.pars.add(f'{key}_center', value=pos, min=np.min(self.energy), max=np.max(self.energy))
+        
+        # Defaults
+        self.pars.add(f'{key}_amplitude', value=np.max(self.counts), min=0.0)
+        self.pars.add(f'{key}_sigma', value=0.2, min=0.05, max=3)
+        self.pars.add(f'{key}_gaussian_sigma', value=0.1, min=1e-3)
+        self.pars.add(f'{key}_gamma', value=0.0, min=1e-3, max=1.0)
 
-    def __call__(self, x, a=0, b=0, m=0):
-        return self.amp*self.__ulrik(x, self.energy, self.FWHM, a, b, m)
-    
 
-class fit:
-    def __init__(self, num):
-        total = [peak()]*num
+    def add_doublet(self, key, pos, delta):
+        self.models[key] = models.ConvGaussianDoniachDublett(prefix=f'{key}_')
+
+        self.pars.add(f'{key}_center', value=pos, min=np.min(self.energy), max=np.max(self.energy))
+        self.pars.add(f'{key}_soc', value=delta)
+        
+        # Defaults
+        self.pars.add(f'{key}_amplitude', value=np.max(self.counts), min=0.0)
+        self.pars.add(f'{key}_sigma', value=0.2, min=0.05)
+        self.pars.add(f'{key}_gaussian_sigma', value=0.1, min=1e-3)
+        self.pars.add(f'{key}_gamma', value=1e-3, min=1e-3, max=1.0)
+        self.pars.add(f'{key}_height_ratio', value=0.5, min=1e-3, max=1.0)
+        self.pars.add(f'{key}_fct_coster_kronig', value=1.0, vary=False)
+        
+
+    def fit(self):
+        # Define model from dictionary
+        for _, obj in self.models.items():
+            try:
+                mod += obj
+            except UnboundLocalError:
+                mod = obj
+        
+        # Run fit
+        x = self.energy
+        y = self.counts
+        out = mod.fit(y, self.pars, y=y, x=x, weights=1 /(np.sqrt(y)))
+
+        return out
     
