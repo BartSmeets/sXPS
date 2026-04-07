eb4438a8a1fcc642a0ea027a8ac5be860a53297f
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2026-04-07 16:29:13 +0200

    plot result

--- core/analysis.py
+++ core/analysis.py
@@ -2,11 +2,22 @@ import numpy as np
 from lmfitxps import models
 import lmfit
 
-class data:
+class Data:
     def __init__(self, file, worksheet):
         '''
         Load data
         '''
+        self.__load_data(file, worksheet)
+
+        # Prepare fit
+        self.models = {'bg': models.ShirleyBG(prefix='shirley_')}   # Add background
+        self.pars = lmfit.Parameters()
+        ## Background parameters
+        self.pars.add('shirley_k', value=0.002, min=0, max=5)
+        self.pars.add('shirley_const', value=self.counts[-1], min=0, max=2*np.max(self.counts))
+
+
+    def __load_data(self, file, worksheet):
         import pandas as pd
         excel = file
         df = pd.read_excel(excel, worksheet)
@@ -26,15 +37,6 @@ class data:
         self.energy = df['Binding Energy (eV)'].to_numpy(dtype=np.float64)
         self.counts = df['Counts / s'].to_numpy(dtype=np.float64)
 
-        # Prepare fit
-        self.models = {'bg': models.ShirleyBG(prefix='shirley_')}   # Add background
-        self.pars = lmfit.Parameters()
-        ## Background parameters
-        self.pars.add('shirley_k', value=0.002, min=0, max=5)
-        self.pars.add('shirley_const', value=self.counts[-1], min=0, max=2*np.max(self.counts))
-
-        return
-
 
     def add_singlet(self, key, pos):
         self.models[key] = models.ConvGaussianDoniachSinglett(prefix=f'{key}_')
@@ -52,7 +54,7 @@ class data:
         self.models[key] = models.ConvGaussianDoniachDublett(prefix=f'{key}_')
 
         self.pars.add(f'{key}_center', value=pos, min=np.min(self.energy), max=np.max(self.energy))
-        self.pars.add(f'{key}_soc', value=delta)
+        self.pars.add(f'{key}_soc', value=delta, vary=False)
         
         # Defaults
         self.pars.add(f'{key}_amplitude', value=np.max(self.counts), min=0.0)
@@ -64,17 +66,46 @@ class data:
         
 
     def fit(self):
+        return FitResult(self)
+    
+
+class FitResult:
+    def __init__(self, data):
+        self.data = data
+        self.result = self.__run_fit()
+        self.comps = self.result.eval_components()
+        
+    
+    def __run_fit(self):
+        data = self.data
         # Define model from dictionary
-        for _, obj in self.models.items():
+        for _, obj in data.models.items():
             try:
                 mod += obj
             except UnboundLocalError:
                 mod = obj
         
         # Run fit
-        x = self.energy
-        y = self.counts
-        out = mod.fit(y, self.pars, y=y, x=x, weights=1 /(np.sqrt(y)))
-
+        x = data.energy
+        y = data.counts
+        out = mod.fit(y, data.pars, y=y, x=x, weights=1 /(np.sqrt(y)))
         return out
     
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        import toml
+
+        settings = toml.load('core\\plot_settings.toml')
+
+        data = self.data
+        comps = self.comps
+        bg = comps['shirley_']
+        plt.plot(data.energy, bg, c='black', label='Shirley background')
+
+        for (key, value), (_, colour) in zip(comps.items(), settings['colours']['core'].items()):
+            if key == 'shirley_':
+                continue
+            else:
+                plt.plot(data.energy, bg+value, label=key[:-1], color=colour)
+                plt.fill_between(data.energy, bg, bg + value, alpha=0.3, color=colour)
